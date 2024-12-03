3cd54fd88f8d6a124af92777d48c6f8f4b98d60f
Ashiq Ahamed <aashiq121205@gmail.com>
2024-12-04 05:15:47 +0530

    Day 1-1 cpp

--- Day-1/solution.cpp
+++ Day-1/solution.cpp
@@ -1,20 +1,26 @@
 #include <bits/stdc++.h>
 #include <vector>
 #define ll long long
-using namespace std;;
+using namespace std;
+
+const int N = 1000;
 
 int main() {
 
-    int n; cin>>n;
-    vector<ll> v1(n);
-    vector<ll> v2(n);
-    for(int i=0; i<n; i++){
-        cin>>v1[i]>>v2[i];
+    vector<ll> v1;
+    vector<ll> v2;
+    for(int i=0; i<N; i++){
+        int x, y;
+        cin>>x>>y;
+        v1.push_back(x);
+        v2.push_back(y);
     }
-    int res = 0;
+    
+    
+    ll res = 0;
     sort(v1.begin(), v1.end());
     sort(v2.begin(), v2.end());
-    for(int i=0; i<n; i++) {
+    for(int i=0; i<N; i++) {
         int a = abs(v1[i] - v2[i]);
         res += a;
     }
